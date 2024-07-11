62b22ad6364059ac4ecb218612c035fd14e56249
DiveshChauhan123 <diveshchauhan792004@gmail.com>
2024-07-11 13:23:24 +0530

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1>HOME</h1>
+      <h1 className='text-white text-3xl underline'>HOME</h1>
+      <Button>Click me</Button>
     </div>
   )
 }
